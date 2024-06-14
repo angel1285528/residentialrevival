743de9b282bf33330aa24b7bb62f78e9a85be399
angel1285528 <lce.angelrdz@gmail.com>
2024-06-13 19:30:16 -0600

    Se inicia proyecto residential revival

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import * as react from 'react'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
